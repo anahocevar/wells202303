fd7a4e0183f7dc24f30b1f15e591ab45dd76a98e
anahocevar <ahocevara@gmail.com>
2023-04-27 18:12:06 +0000

    Improve app wording and some typos

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from plot import plot_wells
 
 def app():
     
-    st.title("This a wells app")
+    st.title("This is a wells app")
 
-    st.markdown("This is text in **bold** and _italic_.")
+    st.markdown("Input the **minimum well depth** as well as the **minimum well gradient** below. _Thank you for using the app!_")
     
     depth = st.number_input("Min depth:", 0, 10000, value=5000, step=200)
     gradient = st.number_input("Min gradient:", 0.0, 0.1, value=0.01, step=0.005)
