35296bfa62362e463e2619ea12accc9af7d09333
anahocevar <ahocevara@gmail.com>
2023-04-27 17:53:48 +0000

    First version of the app

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from database import query_db
+from plot import plot_wells
+
+def app():
+    
+    st.title("This a wells app")
+
+    st.markdown("This is text in **bold** and _italic_.")
+    
+    depth = st.number_input("Min depth:", 0, 10000, value=5000, step=200)
+    gradient = st.number_input("Min gradient:", 0.0, 0.1, value=0.01, step=0.005)
+    
+    data = query_db(depth, gradient)
+    st.write(plot_wells(data))
+
+if __name__ == '__main__':
+    app()
